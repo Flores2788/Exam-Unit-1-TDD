c0ca011c7ffc7b5f60d0d85a0f49759ff401d170
Giorgio Fabbroni <giorgiofabbroni@hotmail.com>
2025-02-02 20:49:50 +0100

    added a fail test putting numbers so it returns null

--- b.mjs
+++ b.mjs
@@ -43,7 +43,7 @@ function formatName(name) {
 
 test("test1").isEqual(formatName("john doe"), "John Doe", "john doe => John Doe");
 test("test2").isEqual(formatName("john"), "John", "john => John");
-
+test("test3").isEqual(formatName("john1   doe2"), "John Doe", "john   doe => John Doe");
 
 
 //#endregion
